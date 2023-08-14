c314986a60d4dd4a20d10555175f59989e22188e
Jane Muthoka <113298792+jane-muthoka@users.noreply.github.com>
2023-08-14 23:38:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -42,11 +42,7 @@ Research = st.number_input('Research:',min_value=0.1, max_value=1.0, value=1.0)
         #np.array([["GRE Score", "TOEFL Score", "University Rating", "SOP", "LOR", "CGPA", "Research"]]))
     #st.success(f'The chance of admit is {result[0]:.2f} ')
 
-# button
 if st.button("Predict chance of Admit"):
     Chance_of_Admit = predict(
-        np.array([[GRE_Score, TOEFL_Score, University_Rating, SOP, LOR, CGPA, Research]]))
-    st.success(f'The chance of admit is {Chance_of_Admit[0][0]:.2f}')  # Use Chance_of_Admit
-
-
-
+        GRE_Score, TOEFL_Score, University_Rating, SOP, LOR, CGPA, Research)
+    st.success(f'The chance of admit is {Chance_of_Admit[0][0]:.2f}')
